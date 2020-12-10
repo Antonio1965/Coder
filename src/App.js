1cad5ec22761633769439fca9fffe91ecd338aff
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2020-12-10 00:07:16 -0300

    Arreglo en componente ListProducts

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './reset.css';
 import  NavBar  from './components/global/NavBar';
 import  Main  from './components/homes/Main';
 import  Footer  from './components/global/Footer';
-import ListProducts from './components/productos/ListProducts';
+import ListProducts from '../src/components/productos/ListProducts';;
 
 function App() {
   return (
     <>
     <div className="App">
     <NavBar/>
-    <ListProducts/>
+    <ListProducts tipo='Tipo de Producto'
+    lista=
+    {[
+      'imagen1',
+      'imagen2',
+      'imagen3',
+      'imagen4',
+      'imagen5',
+      'imagen6',
+      'imagen7',
+      
+    ]}
+    />
     <Footer/>
    
     </div>
