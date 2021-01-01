5e3b9a8a7d71296247800ed1b556f480a31bd796
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-01-01 09:47:03 -0300

    Se modifica en el componente Category

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,19 +1,26 @@
-import {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+
 
 const Category = () => {
     const {category_name} = useParams();
 
-    useEffect(() => {
-        console.log(category_name);
-    }, [category_name])
+    const queCategoriaEs = (cat) => {
+        switch(cat) {
+            case 'home': return <h2 className="container">Estoy en home</h2>;
+            case 'celulares-accesorios': return <h2 className="container">Estoy en Celulares-accesorios</h2>;
+            case 'tables-pc-escritorios': return <h2 className="container">Estoy en tables-pc-escritorios</h2>;
+            case 'dispositivos-auxiliares': return <h2 className="container">Estoy en dispositivos auxiliares</h2>;
+            case 'juegos-didacticos': return <h2 className="container">Estoy en juegos didacticos</h2>;
+            case 'otros': return <h2 className="container">Estoy en otros</h2>
+            default: return <h2 className="container"> la categoría no existe</h2>
+        }
+    }
 
     return (
         <>
-            <div className='container'>
-            <h1>{category_name}</h1>
-            
-            </div>
+            {
+               queCategoriaEs(category_name) 
+            }
         </>
     )
 }
