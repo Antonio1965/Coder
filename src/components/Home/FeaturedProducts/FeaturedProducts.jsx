c272542ce94a2f90af665588b6bb947e4c6b1cad
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-01-01 09:58:48 -0300

    Se modifico en el componente FeatureProducts y ProductCard

--- src/components/Home/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts/FeaturedProducts.jsx
@@ -1,18 +1,76 @@
+import {useState, useEffect} from 'react';
 import ProductCard from '../../general/ProductCard/ProductCard';
 import './FeaturedProducts.css';
 
 const FeaturedProducts = () => {
+    const [items, setItems] = useState([]);
+    // Hagan de cuenta que esta constante es una API
+    const products = [
+        {
+            id: 1,
+            titulo: 'Producto 1',
+            precio: 1500,
+        },
+        {
+            id: 2,
+            titulo: 'Producto 2',
+            precio: 3000,
+        },
+        {
+            id: 3,
+            titulo: 'Producto 3',
+            precio: 6000,
+        },
+        {
+            id: 4,
+            titulo: 'Producto 4',
+            precio: 2500,
+        },
+    ]
+
+    const getProducts = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(products);
+        }, 500)
+    })
+
+    const getProducstFromDB = async () => {
+        try {
+            const result = await getProducts;
+            setItems(result);
+        } catch(error) {
+            alert('No podemos mostrar los productos en este momento');
+        }
+    }
+
+    useEffect(() => {
+        getProducstFromDB();
+    }, [])
+
     return (
         <section className="featuredProducts">
             <div className="container">
-                <h2>Productos destacados</h2>
+                {
+                    items.length ?
+                    <>
+                        <h2>Productos destacados</h2>
 
-                <ul>
-                    <li><ProductCard titulo="Producto 1" precio="1500" /></li>
-                    <li><ProductCard titulo="Producto 2" precio="3000" /></li>
-                    <li><ProductCard titulo="Producto 3" precio="2650" /></li>
-                    <li><ProductCard titulo="Producto 4" precio="2100" /></li>
-                </ul>
+                        <ul>
+                            {
+                                items.map((item, index) => (
+                                    <li key={index}>
+                                        <ProductCard 
+                                            id={item.id}
+                                            titulo={item.titulo} 
+                                            precio={item.precio} 
+                                        />
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </> :
+                    <p className="cargando">Cargando items...</p>
+                }
             </div>
         </section>
     )
