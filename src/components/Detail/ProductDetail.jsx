6ee97cfb0c846a423d9533f08dd0ce70553ec8ff
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-01-07 07:10:56 -0300

    Se modifico ProductDetail y se agrega componente Comments.

--- src/components/Detail/ProductDetail.jsx
+++ src/components/Detail/ProductDetail.jsx
@@ -1,12 +1,45 @@
+import {useState} from 'react';
+import './ProductDetail.css';
+
 const ProductDetail = ({item}) => {
+    const [qty, setQty] = useState(1);	
+
+    const handleClickResta = () => {	
+        if(qty > 1) {	
+            setQty(qty - 1);	
+        }	
+    }	
+
+    const onAdd = () => {	
+        alert(`Agregaste ${qty} productos al carrito`);	
+    }
+
     return (
-        <article>
-            <h1>{item.nombre}</h1>
-            <p>El id de este producto es {item.id}</p>
-            <img src={item.foto} alt=""/>
-            <p>{item.descripcion}</p>
-            <p>{item.precio}</p>
-            <button>Agregar al carrito</button>
+        <article className="product">
+            <div className="foto">
+                <img src="http://placehold.it/400x400" alt=""/>
+            </div>
+
+            <div className="info">
+                <h1 className="title">{item.title}</h1>
+                {
+                    !!item.description && <p className="description">{item.description}</p>
+                }
+                <p className="price">${item.price}</p>
+
+                <div className="qty">	
+                    <button 	
+                        disabled={qty === 1 ? 'disabled' : null } 	
+                        onClick={handleClickResta}	
+                    >	
+                        -	
+                    </button>	
+                    <input type="text" value={qty} readOnly/>	
+                    <button onClick={() => setQty(qty + 1)}>+</button>	
+                </div>
+
+                <button className="btn" onClick={onAdd}>Agregar al carrito</button>
+            </div>
         </article>
     )
 }
