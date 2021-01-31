cfb26608e7db8fcacf0d1bc930680f996635b123
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-01-31 16:05:59 -0300

    Se arreglo el estilo css del home

--- src/components/general/ProductCard/ProductCard.jsx
+++ src/components/general/ProductCard/ProductCard.jsx
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom';
 const ProductCard = ({id, titulo, precio, categoria, type='grid', img}) => {
     return (
         <article className={`productCard ${type}`}>
+            <figure className='contenedor-imagen'>
             <img src={`/products/${img}`}  alt="Mi producto de prueba"/>
+            </figure>
             <div>
                 <h3>{titulo}</h3>
                 <p>${precio}</p>
